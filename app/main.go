e209697077f84aed290c9f8430b4093a688fe5db
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-01-12 20:39:58 +0900

    change

--- app/main.go
+++ app/main.go
@@ -3,9 +3,13 @@ package main
 import ( 
   "fmt"
   "net/http"
-)
+  )
 
 func handler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello, GitOps!")
 }
 
+func main() {
+  http.HandlerFunc("/", handler)
+  http.ListenAndServe(":8080", nil)
+}
